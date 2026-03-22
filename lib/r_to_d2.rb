bc1605bb2f500ca45e7dd31312123f37e088cd53
Klaus Weidinger <weidkl@gmx.de>
2026-03-22 11:28:01 +0100

    bundle gem r_to_d2

---
+++ lib/r_to_d2.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "r_to_d2/version"
+
+module RToD2
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/r_to_d2/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RToD2
+  VERSION = "0.1.0"
+end
